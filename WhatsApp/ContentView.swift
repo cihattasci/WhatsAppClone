6119c14af1ee9674e3d159ac74fbfa461b83f13f
Cihat Tasci <cihattasci97@gmail.com>
2023-04-17 18:33:40 +0300

    added default selection

--- WhatsApp/ContentView.swift
+++ WhatsApp/ContentView.swift
@@ -12,28 +12,36 @@ struct ContentView: View {
         UITabBar.appearance().backgroundColor = .black
         UITabBar.appearance().unselectedItemTintColor = .gray
     }
+    
+    @State private var selection = 4
+    
     var body: some View {
-        TabView{
+        TabView(selection: $selection){
             Situation().navigationBarBackButtonHidden(true)
                 .tabItem{
                     Label("Durum", systemImage: "circle.circle")
                 }
+                .tag(1)
             Calls().navigationBarBackButtonHidden(true)
                 .tabItem{
                     Label("Aramalar", systemImage: "phone")
                 }
+                .tag(2)
             Communities().navigationBarBackButtonHidden(true)
                 .tabItem{
                     Label("Topluluklar", systemImage: "person.3")
                 }
+                .tag(3)
             Chats().navigationBarBackButtonHidden(true)
                 .tabItem{
                     Label("Sohbetler", systemImage: "message")
                 }
+                .tag(4)
             Settings().navigationBarBackButtonHidden(true)
                 .tabItem{
                     Label("Ayarlar", systemImage: "gear")
                 }
+                .tag(5)
         }.tint(.blue)
     }
 }
