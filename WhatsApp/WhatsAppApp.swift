fecdb5e237ca074a9c81f2cff5a1ff8809acce63
Cihat Tasci <cihattasci97@gmail.com>
2023-04-17 16:00:57 +0300

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Cihat Tascı on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhatsAppUITests/WhatsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WhatsAppUITestsLaunchTests.swift
+//  WhatsAppUITests
+//
+//  Created by Cihat Tascı on 17.04.2023.
+//
+
+import XCTest
+
+final class WhatsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
